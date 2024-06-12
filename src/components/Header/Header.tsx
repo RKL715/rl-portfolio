865b8577730fb8dee69fc1e955df1f4d5fe0b10c
RKL715 <remykennyl715@gmail.com>
2024-06-12 15:52:18 +0200

    ## add * improved title h1 + added link to / * stylised button in header + fixed hover effect * improved accessibility with tabindex + &:focus &:active for "hover effect"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { useState} from "react";
 import ContactModal from "../Contact/Contact.tsx";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 
 function Header () {
@@ -20,7 +20,10 @@ const [isVisible, setIsVisible] = useState(true);
     return (
         <header>
             <div className="header-top-block">
-                <h1>Rémy LAFAYE</h1>
+                <h1>
+                    <Link to="/" className="header-link">
+                    RÉMY LAFAYE
+                    </Link></h1>
                 <p>Développeur intégrateur web</p>
             </div>
 
@@ -29,23 +32,25 @@ const [isVisible, setIsVisible] = useState(true);
                     <ul>
                         <li>
                             <button
-                                className="button-project-page" onClick={() => navigate("/infos")}>
+                                className="button-project-page"
+                                onClick={() => navigate("/infos")} tabIndex={0}>
                                 Infos
                             </button>
                         </li>
                         <li>
                             <button
-                                className="button-project-page" onClick={() => navigate("/projects")}>
+                                className="button-project-page"
+                                onClick={() => navigate("/projects")} tabIndex={0}>
                                 Projets
                             </button>
                         </li>
                         <li>
-                            <a href="https://github.com/RKL715" target="_blank" rel="noopener noreferrer">Github</a>
+                            <a href="https://github.com/RKL715" target="_blank" rel="noopener noreferrer" tabIndex={0}>Github</a>
                         </li>
                         <li>
                             <button
                                 className="button-contact-modal"
-                                onClick={openContactModal}>
+                                onClick={openContactModal} tabIndex={0}>
                                 Contact
                             </button>
                         </li>
--- src/components/Project/ProjectCarousel.tsx
+++ src/components/Project/ProjectCarousel.tsx
@@ -12,13 +12,13 @@ function ProjectCarousel({images, nextSlide, prevSlide} : CarouselType) {
   return (
       <div className="carousel">
           <img src={images.url} alt={images.title}/>
-          <button className="prev" onClick={prevSlide}>
+          <button className="prev" onClick={prevSlide} tabIndex={0}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                    stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5"/>
               </svg>
           </button>
-          <button className="next" onClick={nextSlide}>
+          <button className="next" onClick={nextSlide} tabIndex={0}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                    stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
