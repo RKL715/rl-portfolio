731e0a0c882dd41afeed1822c965138238e984c6
RKL715 <remykennyl715@gmail.com>
2024-06-12 15:17:21 +0200

    ## add : * added better button for carousel + animated. * improvment of project page : responsive design

--- src/components/Project/ProjectCarousel.tsx
+++ src/components/Project/ProjectCarousel.tsx
@@ -3,14 +3,29 @@ import type { ImageType } from "./Project.tsx";
 // TS interfaces
 interface CarouselType {
     images : ImageType ;
+    nextSlide : () => void;
+    prevSlide : () => void;
 }
 
-function ProjectCarousel({images} : CarouselType) {
+function ProjectCarousel({images, nextSlide, prevSlide} : CarouselType) {
 
   return (
-    <div className="carousel">
- <img src={images.url} alt={images.title} />
-    </div>
+      <div className="carousel">
+          <img src={images.url} alt={images.title}/>
+          <button className="prev" onClick={prevSlide}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                   stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5"/>
+              </svg>
+          </button>
+          <button className="next" onClick={nextSlide}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                   stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
+              </svg>
+
+          </button>
+      </div>
   );
 }
 
