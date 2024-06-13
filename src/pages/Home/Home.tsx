87ff3f4e1d5a68d05752a5a5a1bc5dc246835798
RKL715 <remykennyl715@gmail.com>
2024-06-13 11:54:50 +0200

    ## added : * home page content + text + responsive + style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,14 @@
 function Home () {
     return (
-        <div>
+        <div className="home">
+            <p className="home-text">
+                Animateur Socioculturel pendant 10 ans, j'ai conclu ma carrière avec des projets d'inclusion numérique pour des personnes en situation d'illectronisme, des projets multimédia pour l'inclusion d'adultes en situation de handicap par le cinéma et le numérique.
+                <br/>
+                Confronté aux difficultés des publics rencontrés pour manipuler le web et les outils numériques et poussé par une restructuration drastique de mon association, j'ai décidé de quitter ma carrière afin de suivre une formation de développeur web pour comprendre l'autre côté du web, en espérant le rendre plus accessible et plus inclusif.
+                <br/>
+                J'ai donc suivi une formation Développeur Intégrateur Web auprès d'OpenClassrooms.
+
+            </p>
         </div>
     )
 }
