98684bba891401df6c5d32469e80c996f1c0c80c
RKL715 <remykennyl715@gmail.com>
2024-06-17 22:23:57 +0200

    ## changed :  * fixed typos in bio text

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,27 +11,28 @@ function Home () {
     return (
         <div className="home">
             {currentPage === 1 && (
-            <p className="home-text" aria-label ="Compétences">
-                Amateur de <b>React</b> et <b>SASS</b>, j'ai également travaillé avec <b>Redux Toolkit</b>,
-                évidemment <b>Git</b> et <b>GitHub</b>.
-                <br/>Actuellement, j'apprends <b>TypeScript</b> pour enrichir mes
-                compétences en <b>JavaScript</b>.
-                <br/><span> J'ai pu travailler sur <b>VSCode</b>, mais <b>WebStorm</b> reste mon favori</span>
-            </p>
+                <p className="home-text" aria-label="Compétences">
+                    Amateur de <b>React</b> et <b>SASS</b>, j'ai également travaillé avec <b>Redux Toolkit</b>.
+                    <br/>Actuellement, j'apprends <b>TypeScript</b> pour enrichir mes
+                    compétences en <b>JavaScript</b>.
+                    <br/><span> Je préfère <b>WebStorm</b> à <b>VSCode</b>.</span>
+                </p>
             )}
             {currentPage === 2 && (
-            <p className="home-text bio" aria-label="Parcours profesionnel">
-                <b>Animateur Socioculturel</b> pendant <b>10 ans</b>, j'ai conclu ma carrière en menant des <b>formations</b>
-                pour des personnes en situation <b>d'illectronisme</b> et des <b>projets multimédia</b> pour <b>l'inclusion
+                <p className="home-text bio" aria-label="Parcours profesionnel">
+                <b>Animateur Socioculturel</b> pendant <b>10 ans</b>, j'ai conclu ma carrière en menant des <b>formations </b>
+                pour des personnes en situation <b>d'illectronisme</b>.
+                    <br/>
+                    J'ai également animé des <b>projets multimédia</b> pour <b>l'inclusion
                     d'adultes</b> en situation de <b>handicap</b> par le <b>cinéma</b> et le <b>numérique</b>.
             </p>
             )}
             {currentPage === 3 && (
                 <p className="home-text bio" aria-label="Parcours professionel deuxième partie">
-                    Suite à la <b>restructuration</b> de mon <b>association</b>, j'ai <b>quitté</b> ma <b>carrière</b> pour me <b>reconvertir</b>
+                    Suite à la <b>restructuration</b> de mon <b>association</b>, j'ai <b>quitté</b> ma <b>carrière</b> pour me <b>reconvertir </b>
                     dans le <b>développement</b>, dans le but de rendre le <b>web</b> plus <b>accessible</b> et <b>inclusif</b>.
                         <br/>
-                    J'ai donc suivi une <b>formation</b> <b>Développeur Intégrateur Web</b> (RNCP 5) auprès d'<b>OpenClassrooms</b>.
+                    J'ai donc suivi une <b>formation</b> <b>Développeur Intégrateur Web</b> (RNCP-5) auprès d'<b>OpenClassrooms</b>.
                 </p>
             )}
             <div className="home-navigation">
