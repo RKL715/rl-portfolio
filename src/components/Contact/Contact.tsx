305a53ffc26be227d6f664577ef128b6134aa540
RKL715 <remykennyl715@gmail.com>
2024-06-17 10:51:17 +0200

    ## changed : * using Web3form instead of formsubmit.co

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,15 +4,18 @@ interface ContactModalProps {
     closeContactModal: () => void;
     isVisible: boolean;
     setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
+
 }
 
 
-function ContactModal ( {closeContactModal, isVisible, setIsVisible} : ContactModalProps) {
+function ContactModal ( {closeContactModal} : ContactModalProps) {
 
     // TO Create a reference to the modal
     const modalRef = useRef<HTMLDivElement>(null);
-    const [isSubmitted, setIsSubmitted] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+    const [isVisible, setIsVisible] = useState(true);
+    // const [isSubmitted, setIsSubmitted] = useState(false);
+    // const [isLoading, setIsLoading] = useState(false);
+    const [result, setResult] = useState('');
 
     // TO Close modal when clicking outside
     useEffect(() => {
@@ -44,16 +47,36 @@ function ContactModal ( {closeContactModal, isVisible, setIsVisible} : ContactMo
     }, [closeContactModal]);
 
     // TO Handle form submission
-    const handleSubmit = (event : React.FormEvent) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        setIsLoading(true);
-        setIsSubmitted(true)
-        setTimeout(() => {
-            setIsVisible(false);
-            closeContactModal();
-            setIsLoading(false);
-        }, 2000);
-    }
+        // setIsLoading(true);
+        setResult('sending');
+
+        const formData = new FormData(event.currentTarget);
+        formData.append("access_key", " 5fe833ef-4663-4411-a3f9-c1e1c9ad4e48");
+
+        const response = await fetch ("https://api.web3forms.com/submit",{
+            method: 'POST',
+            body: formData,
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult('success');
+            event.currentTarget.reset ();
+        } else {
+            setResult(data.message);
+        }
+    };
+
+        // setIsSubmitted(true)
+        // setTimeout(() => {
+        //     setIsVisible(false);
+        //     closeContactModal();
+        //     setIsLoading(false);
+        // }, 2000);
+
 
     // TO Render the contact form
     return (
@@ -62,28 +85,28 @@ function ContactModal ( {closeContactModal, isVisible, setIsVisible} : ContactMo
                 <h2 className="contact-form-title">CONTACT</h2>
             </div>
 
-            <form onSubmit={handleSubmit}
-                  action="https://formsubmit.co/rkl-tech@protonmail.com" method="POST">
-            <div className="contact-form-fields">
-                <label htmlFor="name"></label>
-                <input type="text" id="name" name="name" placeholder="NAME" required/>
+            <form onSubmit={handleSubmit}>
+                <div className="contact-form-fields">
+                    <label htmlFor="name"></label>
+                    <input type="text" id="name" name="name" placeholder="NAME" required/>
 
-                <label htmlFor="email"></label>
-                <input type="email" id="email" name="email" placeholder="E-MAIL" required/>
+                    <label htmlFor="email"></label>
+                    <input type="email" id="email" name="email" placeholder="E-MAIL" required/>
 
-                <label htmlFor="message"></label>
-                <textarea id="message" name="message" placeholder="MESSAGE" required></textarea>
+                    <label htmlFor="message"></label>
+                    <textarea id="message" name="message" placeholder="MESSAGE" required></textarea>
 
-                <button type="submit" className="submit">
-                    {isLoading ?
-                    <div className="loader"></div> :
-                    isSubmitted ?
-                        <img src="/icons/checked.png" alt="Succès d'envoi d'email" className="contact-form-success" width="30"/> :
-                        <img src="/icons/Paper_send.png" alt="Bouton envoyer le mail" width="30"/>
-                    }
-                </button>
-            </div>
+                    <button type="submit" className="submit">
+                        {/*{isLoading ?*/}
+                        {/*<div className="loader"></div> :*/}
+                        {/*isSubmitted ?*/}
+                        {/*    <img src="/icons/checked.png" alt="Succès d'envoi d'email" className="contact-form-success" width="30"/> :*/}
+                        {/*    <img src="/icons/Paper_send.png" alt="Bouton envoyer le mail" width="30"/>*/}
+                        {/*}*/}
+                    </button>
+                </div>
             </form>
+            <span>{result}</span>
         </div>
     )
 }
