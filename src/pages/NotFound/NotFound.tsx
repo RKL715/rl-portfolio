14b99b606ed1f01540f27b0ea0df80669c372fea
RKL715 <remykennyl715@gmail.com>
2024-06-18 12:22:56 +0200

    ## added : * 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,8 +1,8 @@
 function NotFound () {
     return (
-        <div>
-            <h1>404</h1>
-            <p>Page not found</p>
+        <div className="not-found-container">
+            <h2>404</h2>
+            <p>Vous arrivez dans une impasse. Ce n'était sûrement pas ce que vous cherchiez.</p>
         </div>
     )
 }
