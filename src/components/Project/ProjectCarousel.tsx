4593a56f7a83cd8052dd7a07ffcd3c7ffae5d126
RKL715 <remykennyl715@gmail.com>
2024-07-03 10:45:54 +0200

    ## changed : - close button for carousel correctly placed. - skills icon resize with screen witdh (clamp)

--- src/components/Project/ProjectCarousel.tsx
+++ src/components/Project/ProjectCarousel.tsx
@@ -63,14 +63,17 @@ function Modal({src, alt, close, prevSlide, nextSlide} : ModalType) {
 
     return ReactDOM.createPortal (
         <div className="modal" ref={modalRef}>
-            <img src={src} alt={alt}/>
+            <div className="image">
+                <img src={src} alt={alt}/>
+                <button className="close-button" onClick={close}>X</button>
+            </div>
             <button className="slideNav prev" onClick={prevSlide} tabIndex={0} aria-label="Previous slide">
                 <ArrowIcons direction="prev"/>
             </button>
             <button className="slideNav next" onClick={nextSlide} tabIndex={0} aria-label="Next slide">
                 <ArrowIcons direction="next"/>
             </button>
-            <button className="close-button" onClick={close}>X</button>
+
         </div>
         , modalContainer
     );
