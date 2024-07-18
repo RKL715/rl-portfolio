227773a05a77805306a9d288ef1f03bd261ee627
rkl <remykennyl715@gmail.com>
2024-07-18 18:25:03 +0200

    ## Project page : * Grid instead of flex * removed margin and useless code

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,33 +1,10 @@
 import Project, {ProjectType} from "../../components/Project/Project.tsx";
 import projectsData from "../../assets/projects.json";
-import {useEffect, useState} from "react";
-
 
 function ProjectsPage () {
 
-    // TO adjust position of the projects page with the header, depending on the screen width
-    const  [marginTop, setMarginTop] = useState("5rem");
-
-    useEffect(() => {
-        const handleResize = () => {
-            const width = window.innerWidth;
-
-            if (width < 600) {
-                setMarginTop("20rem"); // 20rem to avoid the header, more = less near the header
-            } else {
-                setMarginTop("5rem"); // default value
-            }
-        }
-
-        handleResize();
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize); // cleanup
-        }
-    }, []);
-
     return (
-        <div className={"projects-pages"} style={{ marginTop }}>
+        <div className={"projects-pages"}>
             {projectsData.projects.map((project: ProjectType) => (
                 <Project
                     key={project.id}
