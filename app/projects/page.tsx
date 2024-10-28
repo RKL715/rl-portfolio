4a76eab2566154e23dff01afb5d03d8afb41750a
rkl <remykennyl715@gmail.com>
2024-10-28 14:55:38 +0100

    ## Project list * removed async (useless now) * fixed key placement

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import projectsData from '../../public/assets/projects.json';
 import styles from './ProjectsList.module.scss';
 
 
-export default async function Page() {
+export default function Page() {
 
     const project = projectsData.projects.en;
 
@@ -11,8 +11,8 @@ export default async function Page() {
         <div className={styles.projectsListPage}>
             <div className={styles.projectsListContent}>
                 {project.map((project) => (
-                    <div className={styles.projectsListCards}>
-                    <Link key={project.id} href={`/projects/${project.id}`} passHref>
+                    <div className={styles.projectsListCards} key={project.id}>
+                    <Link href={`/projects/${project.id}`} passHref>
                         <img src={project.img[0].src} alt={project.img[0].title} className={styles.projectImg} />
                         <h2 className={styles.projectsListTitle}>{project.name}</h2>
                     </Link>
